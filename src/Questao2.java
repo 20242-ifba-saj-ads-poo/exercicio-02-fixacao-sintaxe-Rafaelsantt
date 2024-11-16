87fd3b6605b25a01513e4a9f02cedebad8855a56
Rafaelsantt <129911456+Rafaelsantt@users.noreply.github.com>
2024-11-16 18:58:05 +0000

    Q2 -> Resolvida

---
+++ src/Questao2.java
@@ -0,0 +1,11 @@
+//Imprima todos os números de 150 a 300.
+
+public class Questao2 {
+    public static void main(String[] args) throws Exception {
+        int soma = 0;
+        for(int i = 1; i <= 1000; i++) {
+            soma += i;
+        }
+        System.out.println(soma);
+    }
+}
