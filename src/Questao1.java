9c8060488846f9f0bbbc64c9513fd5971ba4515c
Rafaelsantt <129911456+Rafaelsantt@users.noreply.github.com>
2024-11-16 18:40:52 +0000

    Q1 -> Resolvida

--- src/Questao1.java
+++ src/Questao1.java
@@ -1,5 +1,9 @@
+//Imprima todos os números de 150 a 300.
+
 public class Questao1 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        for(int i = 150; i <= 300; i++) {
+            System.out.println(i);
+        }
     }
 }
